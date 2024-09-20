3c8a309e0237822fbfa0fab663456b345894a615
Eric Chee <ericchee@hotmail.co.uk>
2024-09-20 14:41:19 +0100

    implement multiple xcom var example

--- airflow/python_callables/examples/xcom.py
+++ airflow/python_callables/examples/xcom.py
@@ -1,6 +1,8 @@
-def get_name():
-    return 'Eric'
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Eric')
+    ti.xcom_push(key='last_name', value='Chee')
 
 def greet(ti):
-    name = ti.xcom_pull(task_ids='xcom_push_example')
-    print(f"hello {name}")
+    first_name = ti.xcom_pull(task_ids='xcom_push_example', key='first_name')
+    last_name = ti.xcom_pull(task_ids='xcom_push_example', key='last_name')
+    print(f"hello {first_name} {last_name}")
